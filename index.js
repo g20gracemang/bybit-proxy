a3b1d59a251cefe8c63a22f6e8aea377497c66e5
g20gracemang <grace.mang@g20strategies.com>
2026-04-13 10:43:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -611,4 +611,4 @@ const server = http.createServer((req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log("Proxy running on port " + PORT));
+server.listen(PORT, "0.0.0.0", () => console.log("Proxy running on port " + PORT));
